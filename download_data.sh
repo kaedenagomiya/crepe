d5d42c6fd9d755f1c2c26eaf9aebbdf4604c4254
kaedenagomiya <nagomiya75328@gmail.com>
2024-12-12 18:37:29 +0900

    [add] for downloading datas

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+# for CREPE ==============================
+# [x][MIR-1k](http://mirlab.org/dataset/public/)
+wget -P ./data http://mirlab.org/dataset/public/MIR-1K.zip
+unzip ./data/MIR-1K.zip -d ./data
